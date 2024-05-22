ae83479c9a9c6080f7ca6fc6bfe04c718e64260e
vijay2668 <121243884+vijay2668@users.noreply.github.com>
2024-05-22 18:44:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,62 @@ my_bot_token = os.environ['BOT_TOKEN']
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+  """Send a message when the command /start is issued."""
+  user = update.effective_user
+  await update.message.reply_html(
+      rf"Hi {user.mention_html()}!",
+      reply_markup=ForceReply(selective=True),
+  )
+
+
+async def help_command(update: Update,
+                       context: ContextTypes.DEFAULT_TYPE) -> None:
+  """Send a message when the command /help is issued."""
+  await update.message.reply_text("Help!")
+
+
+async def stylize(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+  user_message = update.message.text
+  if user_message is None:
+    await update.message.reply_text("Please send an image to stylize.")
+    return
+
+  img = Image.new('RGB', (500, 200), color=(73, 109, 137))
+  d = ImageDraw.Draw(img)
+  fnt = ImageFont.load_default()
+  d.text((50, 90), user_message, font=fnt, fill=(255, 255, 0))
+
+  img.save('styled_text.png')
+  with open('styled_text.png', 'rb') as photo:
+    await update.message.reply_photo(photo=photo)
+
+
+def main() -> None:
+  """Start the bot."""
+  # Create the Application and pass it your bot's token.
+  application = Application.builder().token(my_bot_token).build()
+
+  # on different commands - answer in Telegram
+  application.add_handler(CommandHandler("start", start))
+  application.add_handler(CommandHandler("help", help_command))
+
+  # on non command i.e message - echo the message on Telegram
+  application.add_handler(
+      MessageHandler(filters.TEXT & ~filters.COMMAND, stylize))
+
+  # Run the bot until the user presses Ctrl-C
+  application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+
+if __name__ == "__main__":
+  main()
